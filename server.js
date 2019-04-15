7d64dfe34887655e61d79fd85e8df85d00558152
ollinieminen <otn987@gmail.com>
2019-04-14 22:40:18 -0700

    make module

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 
 const http = require('http');
+const url = require('url');
 
-http.createServer((request, response)=>{
-    response.writeHead(200, {'Content-Type':'text/plain'});
-    response.write('Hello World');
-    response.end();
-}).listen(8888);
+function start() {
+    function onRequest(request, response) {
+        const pathname = url.parse(request.url).pathname;
+        console.log(`request for ${pathname} received`);
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.write('Hello World');
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
 
+console.log('server has started');
+}
+exports.start = start;
